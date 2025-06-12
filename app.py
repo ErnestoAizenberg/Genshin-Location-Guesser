21a9227cc5555289614b4a13ebf1376d845f4a0e
ErnestoAizenberg <sereernst@gmail.com>
2025-06-12 10:11:48 +0300

    chore: migrated from Flask to FastAPI

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import random
+import logging
 from collections import defaultdict
+from typing import Dict, List, Union
 
-from flask import Flask, jsonify, render_template, request
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.exception_handlers import request_validation_exception_handler
+
+from pydantic import BaseModel
 
 list_of_tasks = [
     {
@@ -43,54 +51,84 @@ list_of_tasks = [
 ]
 
 
-def create_app(app_config=None):
-    app = Flask(__name__)
-    if app_config:
-        app.config.from_object(app_config)  # Fixed: should be app.config.from_object
+class Task(BaseModel):
+    id: Union[str, int]
+    targetImageUrl: str
+
+
+class SubmittedTask(BaseModel):
+    taskId: Union[str, int]
+    guessCoords: List[Union[int, float]]
+    time: Union[int, float]
+
+
+def create_app(app_config=None, logger=None):
+    if not logger:
+        logger = logging.getLogger(__name__)
+        logger.setLevel("DEBUG")
+
+    app = FastAPI()
+    app.state.config = app_config
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+    templates = Jinja2Templates(directory="templates")
 
     region_lookup = defaultdict(list)
     for d in list_of_tasks:
         region_lookup[d.get("region")].append(d)
 
-    @app.route("/dashboard")
-    @app.route("/home")
-    @app.route("/")
-    def index():
-        return render_template("index2.html")
+    @app.exception_handler(Exception)
+    async def error_logger(request: Request, exc: Exception):
+        logger.error(f"{exc.errors()}")
+        return await request_validation_exception_handler(request, exc)
 
-    @app.route("/get_task")
-    def get_task():
-        data = request.args
-        region = data.get("region")
+    @app.get("/", response_class=HTMLResponse)
+    def read_root(request: Request):
+        return templates.TemplateResponse("index2.html", {"request": request})
+
+    @app.get("/get_task", response_model=Task)
+    def get_task(region: str):
 
         if region:
-            dicts_to_select_in = region_lookup[region]
+            dicts_to_select_in: List = region_lookup[region]
         else:
-            random_region = random.choice(list(region_lookup.keys())) if region_lookup else None
+            random_region = (
+                random.choice(list(region_lookup.keys())) if region_lookup else None
+            )
             dicts_to_select_in = region_lookup[random_region] if random_region else []
 
-        task = random.choice(dicts_to_select_in) if dicts_to_select_in else None
-        if task:
-            task_to_send = {
-                "id": task.get("id"),
-                "targetImageUrl": task.get("targetImageUrl"),
-            }
-        else:
-            task_to_send = {}
+        task_dict: Dict | None = (
+            random.choice(dicts_to_select_in) if dicts_to_select_in else None
+        )
+        if task_dict:
+            task_to_send = Task(
+                id=str(task_dict.get("id")),
+                targetImageUrl=task_dict.get("targetImageUrl"),
+            )
+            return task_to_send
+        logger.error(f"No data found for region {region}")
+        raise HTTPException(
+            status_code=404, detail=f"No data found for region {region}"
+        )
 
-        return jsonify(task_to_send)
+    @app.post("/submit_result")
+    def submit_result(submitted_task: SubmittedTask):
 
-    @app.route("/submit_result", methods=["POST"])
-    def submit_result():
-        data = request.get_json()
-        task_id = data.get("taskId")
-        guess_coords = data.get("guessCoords")
-        time_spent = data.get("time")
-        task = next((t for t in list_of_tasks if t["id"] == task_id), None)
-        if task:
-            correct_loc = task.get("correctLocation")
-            return jsonify({"correctLocation": correct_loc})
+        task_id = submitted_task.taskId
+        guess_coords = submitted_task.guessCoords
+        time_spent = submitted_task.time
+
+        task_dict: Dict = next((t for t in list_of_tasks if t["id"] == int(task_id)), None)
+
+        if task_dict:
+            correct_loc = task_dict.get("correctLocation")
+            logger.info(f"correctLocation: {correct_loc}")
+            return {"correctLocation": correct_loc}
         else:
-            return jsonify({"error": "Not found"}), 404
+            logger.error(f"No data found with provided identifier ({task_id})")
+
+            raise HTTPException(
+                status_code=404,
+                detail=f"No data found with provided identifier ({task_id})",
+            )
 
     return app
